7af615245b2a5fea743dd1157f7c7dbd84cd344b
Jacco Taal <jacco@bitnomica.com>
2013-04-25 16:50:35 +0200

    * upgraded to 1.0.0rc3 version of handlebars  * added handlebars.runtime.js file which is available as the js.handlebars.handlebars_runtime Resource  * stripped version information from the static filenames (fanstatic does this for us)

--- js/handlebars/__init__.py
+++ js/handlebars/__init__.py
@@ -1,4 +1,5 @@
 from fanstatic import Library, Resource
 
 library = Library('handlebars', 'resources')
-handlebars = Resource(library, 'handlebars-1.0.rc.1.js')
+handlebars = Resource(library, 'handlebars.js')
+handlebars_runtime = Resource(library, 'handlebars.runtime.js')
--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@
 
 import setuptools
 from setuptools import find_packages
-
+from glob import glob
 setuptools.setup(
   name = 'js.handlebars',
-  version = '1.0.rc.1-1',
+  version = '1.0.0.rc.3',
   license = 'BSD',
   description = 'Fanstatic package for Handlebars.js',
   long_description = open('README.txt').read(),
-  author = 'Matt Good',
-  author_email = 'matt@matt-good.net',
-  url = 'http://github.com/mgood/js.handlebars/',
+  author = 'Jacco Taal',
+  author_email = 'j@jaccotaal.nl',
+  url = 'http://github.com/jrtaal/js.handlebars/',
   platforms = 'any',
   packages=find_packages(),
   namespace_packages=['js'],
   include_package_data=True,
+  data_files = [('js/handlebars/resources',glob('js/handlebars/resources/*.js'))],
   zip_safe = False,
   install_requires=[
     'fanstatic',
